c50888b53b4387562dd883a6272994b9b363f0d7
Amr Mohamed <88450778+Amrabdelfatah0@users.noreply.github.com>
2021-12-18 20:58:04 +0200

    form2

--- input project/main.js
+++ input project/main.js
@@ -1,27 +1,46 @@
 function Login(){
-    var username=$("#UserNameTextv").val();
+    var username=$("UserNameTextv").val();
     var password=$("#PasswordTextv").val();
-
+    
     var formValidation = inputValidation();
     if (formValidation==true) { 
          $("#loginFeedback").show("slow");
-    } else {
+    }
+     else {
         alert("fill inputs first");
     }
 }
+function gon(){
+    var username=$("UserNameTextv").val();
+    var password=$("#PasswordTextv").val();
+    var formValidation = inputValidation();
+     if (formValidation==true) { 
+          $("#modalContent").fadeIn("slow");
+    }
+     else {
+        alert("fill inputs first");
+    }
+
+}
+
+
+
+
 function clearInputs(){
     $("#UserNameTextv").val("");
     $("#PasswordTextv").val("");
 }
 
+
+
+
+
 function inputValidation(){
     var Valid;
     var username=$("#UserNameTextv").val();
     var password=$("#PasswordTextv").val();
-  
-
    
-    if (username!=""&&password) {
+    if (username!=""&&password!="") {
         Valid=true;
     } else {
         Valid=false;
